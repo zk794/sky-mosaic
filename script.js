3939b2341287ae579fbeeb68b69d13efce78f98d
zk794 <zoa.katok@gmail.com>
2022-02-08 21:29:53 -0600

    grid of normal images

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let apiToken = "563492ad6f91700001000001b8220346b4294348a2e4b831be1085bc"
 let imgArr = []
 let nextPage = "https://api.pexels.com/v1/search?query=sky"
 
+let gridSize = 5
+
 async function getImages(url, reps) {
   const res = await fetch(url,{
     headers: {
@@ -18,41 +20,63 @@ async function getImages(url, reps) {
    if (reps > 0) getImages(nextPage, reps-1)
 }
 
-function fillImgArr(numPics) {
-  const iter = Math.ceil(numPics / 15)
+function shuffleArr(array) {
+  let currentIndex = array.length,  randomIndex;
 
-  // for (let i = 0; i < iter; i++) {
-  //   fetch(nextPage,{
-  //     headers: {
-  //       Authorization: apiToken
-  //     }
-  //    })
-  //    .then(res => res.json())
-  //    .then(data => {
-  //      imgArr = imgArr.concat(data.photos)
-  //      console.log(data.photos)
-  //      nextPage = data.next_page
-  //      console.log(data.next_page)
-  //    })
-  // }
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
 }
 
-function getSkyImg() {
-  const sky = document.querySelector('#sky')
-  fetch("https://api.pexels.com/v1/search?query=sky",{
-    headers: {
-      Authorization: apiToken
+function makeImgGrid(gridSize) {
+  const div = document.getElementById("artwork")
+  let cols = document.querySelectorAll("div.column")
+  console.log(`cols.length = ${cols.length}`)
+  while (cols.length < gridSize) {
+    let newCol = document.createElement("div")
+    newCol.setAttribute("class", "column")
+    div.appendChild(newCol)
+    cols = document.querySelectorAll("div.column")
+  }
+  while (cols.length > gridSize) {
+    div.removeChild(document.querySelector("div.column"))
+    cols = document.querySelectorAll("div.column")
+  }
+  console.log(`cols.length = ${cols.length}`)
+  cols.forEach(col => {
+    while (col.children.length < gridSize) {
+      let pic = document.createElement("img")
+      pic.setAttribute("src", "")
+      col.appendChild(pic)
     }
-   })
-     .then(res => res.json())
-     .then(data => {
-       sky.src = data.photos[0].src.medium
-       // console.log(data.photos.length)
-     })
+    while (col.children.length > gridSize) {
+      // remove child
+    }
+  })
+}
+
+function getSkyImg(imgEl, idx) {
+  imgEl.setAttribute("src", imgArr[idx].src.small)
 }
 
 
+getImages(nextPage, 1).then(res => {
+  imgArr = shuffleArr(imgArr)
+  makeImgGrid(gridSize)
+  console.log(imgArr[0])
+  imgs = document.querySelectorAll("img")
+  imgs.forEach((im, idx) => getSkyImg(im, idx))
+})
 
-getImages(nextPage, 2)
-console.log(imgArr.length)
-getSkyImg()
+console.log(document.body.clientWidth)
+console.log(document.body.clientHeight)

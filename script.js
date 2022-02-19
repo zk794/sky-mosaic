4d290287addd36eaf9197ae1d22b6022acaa8d31
zk794 <zoa.katok@gmail.com>
2022-02-19 17:25:33 -0600

    remove hextohue

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let imgArrYel = []
 let imgArrBlu = []
 let imgArrPur = []
 let imgArrPin = []
-let nextPage = "https://api.pexels.com/v1/search?query=clouds"
+let nextPage = "https://api.pexels.com/v1/search?query=clouds&color=red"
 
 const artWidth = Math.floor(document.body.clientWidth * (2/5))
 let gridSize = 10
@@ -32,45 +32,6 @@ async function getImages(url, reps, callback) {
      getImages(nextPage, reps-1, callback)
    }
    else callback()
-}
-
-function hexToHue (hex) {
-  let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-
-    let r = parseInt(result[1], 16);
-    let g = parseInt(result[2], 16);
-    let b = parseInt(result[3], 16);
-
-    r /= 255, g /= 255, b /= 255;
-    let max = Math.max(r, g, b), min = Math.min(r, g, b);
-    let h, s, l = (max + min) / 2;
-
-    if(max == min){
-        h = s = 0; // achromatic
-    } else {
-        let d = max - min;
-        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-        switch(max) {
-            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-            case g: h = (b - r) / d + 2; break;
-            case b: h = (r - g) / d + 4; break;
-        }
-        h /= 6;
-    }
-
-    s = s*100;
-    s = Math.round(s);
-    l = l*100;
-    l = Math.round(l);
-    h = Math.round(360*h)
-
-    if (h <= 13 || h > 337) return "red"
-    if (h >= 298) return "pink"
-    if (h >= 275) return "purple"
-    if (h >= 170) return "blue"
-    if (h >= 70) return "green"
-    if (h >= 53) return "yellow"
-    return "orange"
 }
 
 function shuffleArr(array) {

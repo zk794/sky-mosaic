4729a6a95762eaee261d37b362e9b4abfcb3e424
zk794 <zoa.katok@gmail.com>
2022-02-19 18:54:12 -0600

    reset images on title click

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 
 let apiToken = "563492ad6f91700001000001b8220346b4294348a2e4b831be1085bc"
-let imgArr = []
 let imgArrRed = []
-let imgArrOra = []
-let imgArrYel = []
-let imgArrBlu = []
-let imgArrPur = []
-let imgArrPin = []
-let nextPage = "https://api.pexels.com/v1/search?query=clouds&color=red"
+let imgArr = []
+let nextPageRed = "https://api.pexels.com/v1/search?query=clouds&color=red"
+let nextPage = "https://api.pexels.com/v1/search?query=clouds"
+let arr = []
 
 const artWidth = Math.floor(document.body.clientWidth * (2/5))
 let gridSize = 10
@@ -18,7 +15,8 @@ const imHeight = 650
 
 let canvs = []
 
-async function getImages(url, reps, callback) {
+// load image data into arr from api
+async function getImages(url, url2, reps, callback) {
   const res = await fetch(url,{
     headers: {
       Authorization: apiToken
@@ -27,9 +25,18 @@ async function getImages(url, reps, callback) {
    const data = await res.json()
    imgArr = imgArr.concat(data.photos)
    nextPage = data.next_page
+
+   const res2 = await fetch(url2,{
+     headers: {
+       Authorization: apiToken
+     }
+    })
+    const data2 = await res2.json()
+    imgArrRed = imgArrRed.concat(data2.photos)
+    nextPageRed = data2.next_page
+
    if (reps > 1) {
-     // console.log(imgObj)
-     getImages(nextPage, reps-1, callback)
+     getImages(nextPage, nextPageRed, reps-1, callback)
    }
    else callback()
 }
@@ -49,10 +56,10 @@ function shuffleArr(array) {
   return array;
 }
 
+// set up canvases
 function makeImgGrid(gridSize) {
   const div = document.getElementById("artwork")
   let rows = document.querySelectorAll("div.row")
-  console.log(`rows.length = ${rows.length}`)
   while (rows.length < gridSize) { // add row
     let newRow = document.createElement("div")
     newRow.setAttribute("class", "row")
@@ -64,7 +71,6 @@ function makeImgGrid(gridSize) {
     div.removeChild(document.querySelector("div.row"))
     rows = document.querySelectorAll("div.row")
   }
-  console.log(`rows.length = ${rows.length}`)
   rows.forEach((row, rIdx) => {
     let numCanvs = row.children.length
     while (row.children.length < gridSize) { // add canvases
@@ -83,7 +89,6 @@ function makeImgGrid(gridSize) {
         r = Math.floor(Math.random() * imgArr.length)
         image.setAttribute("src", imgArr[r].src.large)
         image.onload = function(){
-          console.log(`canv ${canv}`)
           ctx.drawImage(image, cropX(canv), cropY(canv),
             canvWidth, canvWidth, 0, 0, canvWidth, canvWidth,);
         }
@@ -117,29 +122,33 @@ function cropY(canvas) {
   return canvWidth * t
 }
 
-function loadInitImages () {
+// load images into canvas grid
+function loadInitImages (arr) {
   canvs.forEach((c, idx) => {
     let ctx = c.getContext('2d')
     const image = new Image()
-    getSkyImg(image, idx)
+    image.setAttribute("src", arr[idx].src.large)
     image.onload = function(){
       ctx.drawImage(image, 0, 0);
     }
   });
 }
 
-function getSkyImg(imgEl, idx) {
-  imgEl.setAttribute("src", imgArr[idx].src.large)
+function reset() {
+  let x = Math.random()
+  if (x < 0.5) {
+    arr = imgArr
+  } else {
+    arr = imgArrRed
+  }
+  loadInitImages(arr)
 }
 
-getImages(nextPage, Math.ceil(gridSize*gridSize/15), () => {
-
-  // const objJSON = JSON.stringify(imgObj);
-  // localStorage.setItem("imgs", objJSON);
-  console.log(imgArr)
+getImages(nextPage, nextPageRed, Math.ceil(gridSize*gridSize/15), () => {
   imgArr = shuffleArr(imgArr)
+  imgArrRed = shuffleArr(imgArrRed)
   makeImgGrid(gridSize)
-  loadInitImages()
+  reset()
 
   setInterval(() => {
     canvs.forEach((c, idx) => {
@@ -147,8 +156,8 @@ getImages(nextPage, Math.ceil(gridSize*gridSize/15), () => {
       if (x <= 0.33) {
         let ctx = c.getContext('2d')
         const image = new Image()
-        r = Math.floor(Math.random() * imgArr.length)
-        image.setAttribute("src", imgArr[r].src.large)
+        r = Math.floor(Math.random() * arr.length)
+        image.setAttribute("src", arr[r].src.large)
         image.onload = function(){
           const xDraw = Math.random() * canvWidth*5/4 - (canvWidth/2)
           const yDraw = Math.random() * canvWidth*5/4 - (canvWidth/2)
